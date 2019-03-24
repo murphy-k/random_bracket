3e26fa156803436ba4158e1f464d90c6c1442921
murphy-k <kyleevanmurphy@gmail.com>
2019-03-24 13:12:18 -0400

    nitpipcking format

--- tournamenttrials.R
+++ tournamenttrials.R
@@ -13,8 +13,6 @@ results <-
 results <-
   `colnames<-`(results, c("Simulation Number", "Winning Team"))
 
-
-
 print(paste(tail(names(sort(
   table(results$`Winning Team`)
 )), n = 5), "has won the most out of all simulations"))
